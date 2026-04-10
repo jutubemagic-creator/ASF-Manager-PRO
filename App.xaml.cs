47c88d82b027ceb85d2741f94be6ae01f732881a
jutubemagic-creator <jutubemagic@gmail.com>
2026-04-10 18:24:40 +0300

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,5 +4,14 @@ namespace ASFManagerPRO
 {
     public partial class App : Application
     {
+        public App()
+        {
+            // Это помогает при запуске
+            DispatcherUnhandledException += (sender, e) =>
+            {
+                MessageBox.Show("Ошибка запуска: " + e.Exception.Message, "ASF Manager PRO", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true;
+            };
+        }
     }
 }
