7dcb007d6cd7531195d99597ea7436add96e98e8
jutubemagic-creator <jutubemagic@gmail.com>
2026-04-11 00:46:52 +0300

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,5 +4,14 @@ namespace ASFManagerPRO
 {
     public partial class App : Application
     {
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Принудительно сохраняем всё при выходе из приложения
+            if (Application.Current.MainWindow is MainWindow mainWindow)
+            {
+                mainWindow.SaveAccounts();
+            }
+            base.OnExit(e);
+        }
     }
 }
