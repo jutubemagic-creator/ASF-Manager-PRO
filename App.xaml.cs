ab24b58a4883f7b3befbb41b43f23e83ed9fea99
jutubemagic-creator <jutubemagic@gmail.com>
2026-04-11 21:19:54 +0300

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,24 @@ namespace ASFManagerPRO
             }
             base.OnExit(e);
         }
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            // Обработка необработанных исключений
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                MessageBox.Show($"Критическая ошибка: {args.ExceptionObject}", "ASF Manager PRO", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            };
+            
+            DispatcherUnhandledException += (sender, args) =>
+            {
+                MessageBox.Show($"Ошибка: {args.Exception.Message}", "ASF Manager PRO", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                args.Handled = true;
+            };
+        }
     }
 }
